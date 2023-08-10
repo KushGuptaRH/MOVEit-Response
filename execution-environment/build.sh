60df09995975ee597ac82fb7cde6a92003adb4a2
Guptanator-boop <kushalgupta@gmail.com>
2023-08-10 19:33:17 -0400

    update test

--- execution-environment/build.sh
+++ execution-environment/build.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 # podman machine start
+# echo 'podman machine started'
 
 ansible-galaxy collection install kushguptarh.moveit_response --force
-
-echo 'podman machine started'
+echo 'colleciton installed locally'
 
 ansible-builder build --tag moveit_ee:latest
-
 echo 'new ee built'
 
 podman push localhost/moveit_ee:latest quay.io/kugupta/moveit_ee:latest
-
 echo 'image pushed to quay'
 
-podman image prune -a -f
-
-echo 'podman artifacts cleaned'
+# podman image prune -a -f
+# echo 'podman artifacts cleaned'
 
 # podman machine stop
-
 echo 'see ya next bug'
